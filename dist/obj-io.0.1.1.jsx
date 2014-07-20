4043b840240a261e775daf9d580628cd499177b2
fabiantheblind <icke@fabianmoronzirfas.me>
2014-07-20 17:50:49 +0200

    minor changes. There is a bug to remember me where I was

--- dist/obj-io.0.1.1.jsx
+++ dist/obj-io.0.1.1.jsx
@@ -1,6 +1,6 @@
 (function(thisObj) {
 
-/*! obj-io.jsx - v0.1.1 - 2014-07-14 */
+/*! obj-io.jsx - v0.1.1 - 2014-07-15 */
 // Read/Write WAVEFRONT OBJ files to disk
 // - single frame or sequence
 // - vertex only!
@@ -564,18 +564,48 @@ function OBJLoader() {
   };
 }
 var import_obj = function(objio) {
-
-  if(objio.import.sequence === false){
-  objio.import.textlines = filereader();
-  if (DEBUG) $.writeln(objio.import.textlines);
-  loader = new OBJLoader();
-  loader.parse(objio.import.textlines.join("\n"));
-  var num = (loader.geometrys[0].positions.length/3);
-  alert("OBJ with "+num +" verticies\n"+loader.geometrys.toSource());
-
-  }else{
-    alert("this should be multi file import");
+    var curComp = app.project.activeItem;
+  if (!curComp || !(curComp instanceof CompItem)) {
+    alert('please select a comp');
+    return;
   }
+
+  if (objio.import.sequence === false) {
+
+    var textlinesArr = filereader();
+    objio.import.textlines.push(textlinesArr);
+    if (DEBUG) $.writeln(objio.import.textlines);
+
+  } else {
+    // alert("this should be multi file import");
+    var textlinesArrs = multifilereader();
+    for (var i = 0; i < textlinesArrs.length; i++) {
+      objio.import.textlines.push(textlinesArrs[i]);
+    }
+  }
+  // loader = new OBJLoader();
+  // loader.parse(objio.import.textlines[0].join("\n"));
+  // var num = (loader.geometrys[0].positions.length/3);
+  // alert("OBJ with "+num +" verticies\n"+loader.geometrys.toSource());
+
+  // at this point we should have:
+  //
+  // an Array or multiple Arrays of text line Arrays in objio.import.textlines
+
+  var loaders = []; // will hold all the obj parsers
+  for (var j = 0; j < objio.import.textlines.length; j++) {
+    var loader = new OBJLoader();
+    loader.parse(objio.import.textlines[j].join("\n"));
+    loaders.push(loader);
+    // now we should have 'n' OBJLoader objects with 'n' verticies
+    // we can start creating the content of the comp
+  }
+
+
+  app.beginUndoGroup('OBJ Import Place || Objects');
+  // only work in the current comp for the moment
+
+  app.endUndoGroup();
 };
 
 // exporter.jsx
--- src/objio/importer.jsx
+++ src/objio/importer.jsx
@@ -1,14 +1,46 @@
 var import_obj = function(objio) {
-
-  if(objio.import.sequence === false){
-  objio.import.textlines = filereader();
-  if (DEBUG) $.writeln(objio.import.textlines);
-  loader = new OBJLoader();
-  loader.parse(objio.import.textlines.join("\n"));
-  var num = (loader.geometrys[0].positions.length/3);
-  alert("OBJ with "+num +" verticies\n"+loader.geometrys.toSource());
-
-  }else{
-    alert("this should be multi file import");
+    var curComp = app.project.activeItem;
+  if (!curComp || !(curComp instanceof CompItem)) {
+    alert('please select a comp');
+    return;
   }
+
+  if (objio.import.sequence === false) {
+
+    var textlinesArr = filereader();
+    objio.import.textlines.push(textlinesArr);
+    if (DEBUG) $.writeln(objio.import.textlines);
+
+  } else {
+    // alert("this should be multi file import");
+    var textlinesArrs = multifilereader();
+    for (var i = 0; i < textlinesArrs.length; i++) {
+      objio.import.textlines.push(textlinesArrs[i]);
+    }
+  }
+  // loader = new OBJLoader();
+  // loader.parse(objio.import.textlines[0].join("\n"));
+  // var num = (loader.geometrys[0].positions.length/3);
+  // alert("OBJ with "+num +" verticies\n"+loader.geometrys.toSource());
+
+  // at this point we should have:
+  //
+  // an Array or multiple Arrays of text line Arrays in objio.import.textlines
+
+  var loaders = []; // will hold all the obj parsers
+  for (var j = 0; j < objio.import.textlines.length; j++) {
+    var loader = new OBJLoader();
+    loader.parse(objio.import.textlines[j].join("\n"));
+    loaders.push(loader);
+    // now we should have 'n' OBJLoader objects with 'n' verticies
+    // we can start creating the content of the comp
+  }
+
+
+  app.beginUndoGroup('OBJ Import Place || Objects');
+  // only work in the current comp for the moment
+  for(var k = 0; k < ){
+
+  }
+  app.endUndoGroup();
 };
